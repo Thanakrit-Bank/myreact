3a28b32e9c5166d0fb803142c72605af6f9a4a98
Thanakrit-Bank <s6201012620091@email.kmutnb.ac.th>
2022-08-16 00:02:37 +0700

    Add dropdown to select province

--- src/components/MapGeojson.jsx
+++ src/components/MapGeojson.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Map, TileLayer} from "react-leaflet";
+import { Dropdown, DropdownButton } from 'react-bootstrap';
 import GeojsonLayer from './GeojsonLayer';
-import Province from './Province'
 import '../css/Map.css';
 
 class MapComponent extends React.Component {
@@ -10,12 +10,6 @@ class MapComponent extends React.Component {
     lng: 100.6,
     zoom: 6.5,
   };
-
-  provinceInput = (lat) => {
-    this.setState({
-      lat: lat,
-    });
-  }
 
   render() {
     var center = [this.state.lat, this.state.lng];
@@ -26,9 +20,15 @@ class MapComponent extends React.Component {
         {/* <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url={basemapsDict[this.state.basemap]}
-        /> */}      
+        /> */}
+
         <GeojsonLayer url="thailand.json" />
-        <Province input={this.provinceInput} />
+
+        <DropdownButton id="test" title="Select Province">
+            <Dropdown.Item href="test">Action</Dropdown.Item>
+            <Dropdown.Item href="test">Another action 555555555</Dropdown.Item>
+            <Dropdown.Item href="test">Something else</Dropdown.Item>
+        </DropdownButton>
       </Map>
     );
   }
